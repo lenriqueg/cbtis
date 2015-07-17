7e29b3aeee7b2d59737d669a698cd49afbc78fba
Luis Enrique <elektroacustica@gmail.com>
2015-07-17 16:20:16 -0500

    Mejorado UI modulo semestres

--- app/views/semestre/create.blade.php
+++ app/views/semestre/create.blade.php
@@ -4,20 +4,32 @@
 
 <section class="container">
 	<div class="row">
-		<div class="col-md-3"></div>
-		<div class="col-md-6">
-			{{ Form::open(['route' => 'sem.new'], ['class' => 'form']) }}
-			<div class="form-group">
-				{{ Form::label('semestre', 'Semestre') }}
-				{{ Form::text('semestre', null, ['class' => 'form-control', 'placeholder' => 'semestre']) }}
-				{{ $errors->first('semestre', '<p class="text-danger">:message</p>')}}
+		<div class="col-md-1"></div>
+		<div class="col-sm-6 col-md-6">
+			<div class="panel panel-primary">
+				<div class="panel-heading">Nuevo semestre</div>
+				<div class="panel-body">
+					{{ Form::open(['route' => 'sem.new'], ['class' => 'form']) }}
+					<div class="form-group">
+						{{ Form::label('semestre', 'Semestre') }}
+						{{ Form::text('semestre', null, ['class' => 'form-control', 'placeholder' => 'semestre']) }}
+					</div>
+					<div class="form-group">
+						<button class="btn btn-primary">Guardar</button>
+					</div>
+					{{ Form::close() }}
+				</div>
 			</div>
-			<div class="form-group">
-				<button class="btn btn-info">Guardar</button>
-			</div>
-			{{ Form::close() }}
 		</div>
-		<div class="col-md-3"></div>
+		<div class="col-sm-6 col-md-4">
+			<div class="panel panel-danger">
+				<div class="panel-heading">Errores</div>
+				<div class="panel-body text-danger">
+					{{ $errors->first('semestre', ':message')}}
+				</div>
+			</div>
+		</div>
+		<div class="col-md-1"></div>
 	</div>
 </section>
 
