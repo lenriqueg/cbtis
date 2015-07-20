531d1d99b857b79701e85ab721d1d61f3219c7a3
luis enrique guatemala martinez <luis@MBP-de-luis.lan>
2015-07-20 13:45:18 -0500

    config de app/bd

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'itssat',
+			'database'  => 'cbtis',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,6 +4,9 @@ class HomeController extends BaseController {
 
 	public function index()
 	{
+		if (Request::ajax()) {
+			return 'ajax';
+		}
 		return View::make('hello');
 	}
 
